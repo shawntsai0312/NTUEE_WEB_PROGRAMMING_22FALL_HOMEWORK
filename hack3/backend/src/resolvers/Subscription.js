26e9cfe898055e5924b2002030e7f24668859750
shawntsai0312 <shawntsai0312@gmail.com>
2022-12-20 11:54:02 +0800

    dead

---
+++ hack3/backend/src/resolvers/Subscription.js
@@ -0,0 +1,23 @@
+const Subscription = {
+  itemCreated: {
+    subscribe: (parent, args, { pubSub }) => {
+      return pubSub.subscribe('ITEM_CREATED');
+    },
+  },
+
+  itemUpdated: {
+    subscribe: (parent, args, { pubSub }) => {
+      return pubSub.subscribe('ITEM_UPDATED');
+    },
+  },
+  // TODO 6.2 Define the itemDeleted subscription resolver
+  itemDeleted: {
+    subscribe: (parent, args, { pubSub }) => {
+      console.log("subD")
+      return pubSub.subscribe('ITEM_DELETED');
+    }
+  }
+  // TODO 6.2 End
+};
+
+export default Subscription;
